511c61a5d3f9f834b515ba7acd92d8827113f3a3
mohamed-mahrous <xxspider6@gmail.com>
2024-06-02 20:30:45 +0300

    use waitress serve

--- wishper-api-server.py
+++ wishper-api-server.py
@@ -62,5 +62,5 @@ def transcript():
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="10.105.173.63", port=8080)
     # app.run(host='localhost',debug=True, port=5000)
