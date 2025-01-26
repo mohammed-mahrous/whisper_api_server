ff82bb79fe79a570735e1fc2346b2f0ae1b93b8a
mohamed-mahrous <xxspider6@gmail.com>
2025-01-26 19:58:05 +0200

    use-aac-format

--- wishper-api-server-aac.py
+++ wishper-api-server-aac.py
@@ -17,7 +17,7 @@ model = FasterWhisperASR(lan=tgt_lan, modelsize=model_size,device='cuda')
 
 def exportFile(file):
     seg:AudioSegment = AudioSegment.from_file(file.stream, 'aac')
-    exported = seg.export('temp-file-{}.wav'.format(getFormattedDate(),format="wav"))
+    exported = seg.export('temp-file-{}.wav'.format(getFormattedDate()),format="wav", parameters=["-ar", "16000", "-ac", "1"])
 
     return exported
 
